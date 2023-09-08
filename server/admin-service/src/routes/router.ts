80083ed429bdd70cf4edc7d8e54ab6d394e6bc3f
LalithYagnavalkya <lalluyagnavalkya.t@gmail.com>
2023-09-09 00:54:34 +0530

    add api planning for admin service

--- server/admin-service/src/routes/router.ts
+++ server/admin-service/src/routes/router.ts
@@ -3,11 +3,24 @@ import { Router } from "express";
 // import superAdminRoutes from './superAdmin.route'
 const router = Router();
 
-// Google auth routes
-// router.use("/", authRoutes);
+// API bulk upload api 
+// this api should take list of users and store them in db
 
-// router.use('/superAdmin', superAdminRoutes )
+// under /customer route
+// API under  update user details 9 such as edit name)
+// API update usesr measurements 
+// add user email or phone number
+// update payment
+// deconsinged user
+
+// under /customerSearch route
+// search with email or phone or some unique id given by admin
+// use measurements to filter customers
 
 
+// under /dashboard
+// api which show due customers in comming days
+// api which shows paid customers in past days
+// api which shows new joinees this month
 
 export default router;
