ca858b2c32216e2df703125b0df859e4e86e15d7
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2023-10-02 18:39:27 +0530

    🍕 update store with new user slice

--- web/src/app/store.ts
+++ web/src/app/store.ts
@@ -1,10 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { apiSlice } from '../features/api/api.slice';
+import userSlice from "@/features/auth/user.slice";
+
+
+const rootReducer = combineReducers({
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    user: userSlice, // Include the authentication slice reducer
+});
 
 export const store = configureStore({
-    reducer: {
-        [apiSlice.reducerPath]: apiSlice.reducer
-    },
+    reducer: rootReducer,
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true
