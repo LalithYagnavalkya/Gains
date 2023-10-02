6195c28442458bc9406d62a91696408041b96905
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2023-10-02 23:19:09 +0530

    🌟add login details to local

--- web/src/features/auth/user.slice.ts
+++ web/src/features/auth/user.slice.ts
@@ -1,11 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const userJSON = localStorage.getItem("currentUser");
+const user = userJSON ? JSON.parse(userJSON) : null;
+
+
 const userSlice = createSlice({
     name: 'user',
     initialState: {
-        isAuthenticated: false,
-        user: null,
-        token: null,
+        isAuthenticated: user? true: false,
+        user: user ? user.user : null,
+        token: user? user.token: null,
     },
     reducers: {
         setAuth(state, action) {
