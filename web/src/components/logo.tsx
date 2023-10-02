803edde0f182afb3e204e96a0696f7b10537dd1c
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2023-10-03 00:24:02 +0530

    ⭐Add logo component

---
+++ web/src/components/logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo: React.FC = () => {
+  return (
+      <div className="flex pl-2 text-start">
+          <img src="/full-logo.svg" alt="" className='h-11' />
+      </div>
+  )
+}
+
+export default Logo
