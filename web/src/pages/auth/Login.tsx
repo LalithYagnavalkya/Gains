5206bf8d861101dabb8d62a3cb5ed8e7735a2b4e
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2023-10-03 00:24:41 +0530

    🔥 remove unwanted code

--- web/src/pages/auth/Login.tsx
+++ web/src/pages/auth/Login.tsx
@@ -6,9 +6,6 @@ import { Card } from '@/components/ui/card'
 const Login: React.FC = () => {
     return (
         <div className='overflow-hidden h-screen'>
-            {/* <h1 className="scroll-m-20 pt-6 pl-5 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-                Gains
-            </h1> */}
             <div className="overflow-hidden h-full relative flex-col items-center justify-center">
                 <div className="lg:p-8 flex h-full">
                     <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
