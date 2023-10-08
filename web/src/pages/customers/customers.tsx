c2e96cc5fa448c682049fcc40b151c153e8aeeb2
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2023-10-08 16:13:05 +0530

    ⚡add Customer page

--- web/src/pages/customers/customers.tsx
+++ web/src/pages/customers/customers.tsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import { Input } from "@/components/ui/input"
+import { Payment, columns } from "./columns"
+import { DataTable } from "./data-table"
+import { MixerHorizontalIcon } from "@radix-ui/react-icons"
+import { Button } from "@/components/ui/button"
+import React from "react"
+import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu"
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { DataTableDemo } from "./table/table"
+import DemoPage from "./customerTable/customer.page"
 
-export const Customer: React.FC  = () => {
-  return (
-    <div>customers</div>
-  )
+type Checked = DropdownMenuCheckboxItemProps["checked"]
+
+export const Customer = () => {
+  const [showStatusBar, setShowStatusBar] = React.useState<Checked>(true)
+  const [showActivityBar, setShowActivityBar] = React.useState<Checked>(false)
+  const [showPanel, setShowPanel] = React.useState<Checked>(false)
+
+  return <div className="p-4 ">
+    <div className="mx-auto container flex justify-between items-center">
+      <div className="items-center w-1/3"><Input type="email" placeholder="Search with names" /></div>
+      <div> <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="outline"
+            size="sm"
+            className="ml-auto h-8 lg:flex"
+          >
+            <MixerHorizontalIcon className="mr-2 h-4 w-4" />
+            View
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="sm">
+          <DropdownMenuLabel>Toggle Columns</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuCheckboxItem
+            checked={showStatusBar}
+            onCheckedChange={setShowStatusBar}
+          >
+            Status Bar
+          </DropdownMenuCheckboxItem>
+          <DropdownMenuCheckboxItem
+            checked={showActivityBar}
+            onCheckedChange={setShowActivityBar}
+          >
+            Activity Bar
+          </DropdownMenuCheckboxItem>
+          <DropdownMenuCheckboxItem
+            checked={showPanel}
+            onCheckedChange={setShowPanel}
+          >
+            Panel
+          </DropdownMenuCheckboxItem>
+        </DropdownMenuContent>
+      </DropdownMenu></div>
+    </div>
+    <div className="mx-auto container">
+      <DemoPage />
+    </div>
+  </div >
 }
