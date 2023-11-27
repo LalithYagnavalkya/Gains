30e95d04a32c67ce086f3abc91262908baee08ff
Lalith <lalluyagnavalkya.t@gmail.com>
2023-11-27 10:39:39 +0530

    🔨 update payment due model

--- server/admin-service/src/models/paymentdue.model.ts
+++ server/admin-service/src/models/paymentdue.model.ts
@@ -1,28 +1,29 @@
 import mongoose, { Document, Model, Schema } from "mongoose";
 
-export interface IPaymentDue extends Document {
+export interface IMembership extends Document {
     userId: Schema.Types.ObjectId;
     transactionId?: Schema.Types.ObjectId;
     paymentType: String,
     status: String,
 }
 
-const paymentDueSchema = new mongoose.Schema(
+const membershipSchema = new mongoose.Schema(
     {
         userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-        transactionId: { type: Schema.Types.ObjectId, ref: 'Transaction' },
-        status: { type: String, enum: ['PENDING', 'PAID', 'UPCOMMING'], default: 'PENDING' },
+        lastPaymentTransactionId: { type: Schema.Types.ObjectId, ref: 'Transaction' },
+        status: { type: String, enum: ['PENDING', 'PAID', 'UPCOMMING', 'PAST_DUE'], default: 'PENDING' },
+        membershipFee: { type: Number, required: true },
         // Assume user is in middle of the month October 15th, and user next payment is on november 1st.
-		// user payment status will be paid for october month. 
-		// On October 23rd, seven days before the validUpto. user paymentStatus will become UPCOMMING_DUE
-		// if user did not pay on 1st, paymentStatus will be PENDING.
+        // user payment status will be paid for october month. 
+        // On October 23rd, seven days before the validUpto. user paymentStatus will become UPCOMMING
+        // if user did not pay on 1st, paymentStatus will be PENDING.
         active: { type: Boolean, required: true, default: true },
-        dueDate: { type: Date, required: true }
+        validUpto: { type: Date, required: true }
     },
     { timestamps: true },
 );
 
-const PaymentDue: Model<IPaymentDue> = mongoose.model<IPaymentDue>("PaymentDue", paymentDueSchema);
+const PaymentDue: Model<IMembership> = mongoose.model<IMembership>("Membership", membershipSchema);
 
 export default PaymentDue;
 
