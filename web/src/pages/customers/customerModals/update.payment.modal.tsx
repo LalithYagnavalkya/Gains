3a56981005c64da088167487d81a13b1c38f6cce
LalithYagnavalkya <lalluyagnavalkya.t@gmail.com>
2023-11-27 07:48:10 +0530

    🎉 Initialize update paymnet modal

--- web/src/pages/customers/customerModals/update.payment.modal.tsx
+++ web/src/pages/customers/customerModals/update.payment.modal.tsx
@@ -1,37 +1,28 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
 
 type Payment = any
 
 interface UpdatePaymentModalProps {
     payment: Payment; // Assuming Payment is a type/interface you have defined
-}
-const UpatePaymentModal: React.FC<UpdatePaymentModalProps> = ({ payment }) => {
-    const dispatch = useDispatch();
-    const [isModalOpen, setModalOpen] = useState<Boolean>(false);
-    const openModal = () => {
-        setModalOpen(true);
-    };
+    togglePaymentModal: any,
 
-    const closeModal = () => {
-        // form.reset();
-        setModalOpen(false);
-    };
-    return <div onClick={openModal}>
-        Update Payment
-        {isModalOpen && (<div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-            <Card className="w-[545px]">
-                <CardHeader>
-                    <CardTitle>Add Customer</CardTitle>
-                    <CardDescription>Fill up your customer details. Click confirm when you're done.</CardDescription>
-                </CardHeader>
-                <CardContent>
-                   
-                </CardContent>
-            </Card>
-        </div>)}
+}
+const UpatePaymentModal: React.FC<UpdatePaymentModalProps> = ({ payment,togglePaymentModal }: any) => {
+
+    return <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+        <Card className="w-[545px]">
+            <CardHeader>
+                <CardTitle>Add Customer</CardTitle>
+                <CardDescription>Fill up your customer details. Click confirm when you're done.</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div className="flex justify-between">
+                    <Button variant="outline" onClick={() => togglePaymentModal(false)} >Cancel</Button>
+                    <Button type="button">Confirm</Button>
+                </div>
+            </CardContent>
+        </Card>
     </div>
 }
 export default UpatePaymentModal;
--- web/src/pages/customers/table/table.tsx
+++ web/src/pages/customers/table/table.tsx
@@ -22,6 +22,21 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
+import { format } from 'date-fns'
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { DotsHorizontalIcon } from "@radix-ui/react-icons"
+import { ColumnDef } from "@tanstack/react-table"
+import { StatusPill } from '@/components/status.pill'
+import UpatePaymentModal from '../customerModals/update.payment.modal'
+
 
 export type Payment = {
     id: string
@@ -29,11 +44,10 @@ export type Payment = {
     status: "pending" | "processing" | "success" | "failed"
     email: string
 };
-import { columns } from './columns'
 import { useGetCustomersQuery } from "@/features/customer/customer.api";
 
-export function DataTableDemo() {
-   
+export function DataTableDemo({paymentModal, togglePaymentModal, SetPaymentModalData } : any) {
+
     const [sorting, setSorting] = React.useState<SortingState>([])
     const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
         []
@@ -63,12 +77,69 @@ export function DataTableDemo() {
         [pageIndex, pageSize]
     )
 
-    // const updateModal
+    const columns: ColumnDef<Payment>[] = [
+        {
+            accessorKey: "username",
+            header: "Name",
+            cell: ({ row }) => (
+                <div className="capitalize">{row.getValue("username")}</div>
+            ),
+        },
+        {
+            accessorKey: "phone",
+            header: ({ column }) => {
+                return (
+                    <>Phone</>
+                )
+            },
+            cell: ({ row }) => <div className="lowercase text-left">{row.getValue("phone")}</div>,
+        },
+        {
+            accessorKey: "paymentStatus",
+            header: "Status",
+            cell: ({ row }) => <div className="">
+                <StatusPill status={row.getValue("paymentStatus")} />
+            </div>,
+        },
+        {
+            accessorKey: "validUpto",
+            header: "Due Date",
+            cell: ({ row }) => <div className="">{format(new Date(row.getValue("validUpto")), 'MMM dd yyy')}</div>,
+        },
+        {
+            id: "actions",
+            enableHiding: false,
+            cell: ({ row }) => {
+                const rowData = row.original
 
 
-    
+                return (
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="ghost" className="h-8 w-8 p-0">
+                                <span className="sr-only">Open menu</span>
+                                <DotsHorizontalIcon className="h-4 w-4" />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                            <DropdownMenuItem
+                            onClick={() => {togglePaymentModal(true); SetPaymentModalData(rowData)}}
+                            >
+                                Update Payment
+                            </DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>View customer</DropdownMenuItem>
+                            <DropdownMenuItem>View payment details</DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                )
+            },
+        },
+    ]
+
     const table = useReactTable({
-        data: data?.users? data.users : defaultData,
+        data: data?.users ? data.users : defaultData,
         columns,
         pageCount: data?.users?.totalCount ?? -1,
         onSortingChange: setSorting,
@@ -89,7 +160,7 @@ export function DataTableDemo() {
             pagination,
         },
     })
-    
+
     return (
         <div className="py-6">
             <div className="rounded-md border">
