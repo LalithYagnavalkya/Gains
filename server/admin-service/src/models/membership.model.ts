c2f1dbd349ed53ab91acbba478430ec29d19a85e
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2023-11-30 18:04:48 +0530

    ⭐ add upate payment form

--- server/admin-service/src/models/membership.model.ts
+++ server/admin-service/src/models/membership.model.ts
@@ -8,6 +8,7 @@ export interface IMembership extends Document {
     membershipDuriation: number,
     validUpto: Date,
     active: true,
+    lastPaymentDate: Date,
 }
 
 const membershipSchema = new mongoose.Schema(
@@ -16,7 +17,8 @@ const membershipSchema = new mongoose.Schema(
         lastPaymentTransactionId: { type: Schema.Types.ObjectId, ref: 'Transaction' },
         paymentStatus: { type: String, enum: ['PENDING', 'PAID', 'UPCOMMING', 'PAST_DUE'], default: 'PENDING' },
         membershipFee: { type: Number, required: true },
-        membershipDuriation: {type: Number, required: true},
+        membershipDuriation: { type: Number, required: true },
+        lastPaymentDate: { type: Date },
         // Assume user is in middle of the month October 15th, and user next payment is on november 1st.
         // user payment status will be paid for october month. 
         // On October 23rd, seven days before the validUpto. user paymentStatus will become UPCOMMING
