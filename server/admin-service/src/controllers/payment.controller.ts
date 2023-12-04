c757236ab7b29ceddd6d46db060dcd425986b8ee
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2023-12-04 14:33:40 +0530

    🎨 Refactor updateMembership API

--- server/admin-service/src/controllers/payment.controller.ts
+++ server/admin-service/src/controllers/payment.controller.ts
@@ -9,10 +9,11 @@ import Transaction from "../models/transaction.model";
 //services
 
 export const updateMembership = async (req: Request, res: Response) => {
-
+try {
+    
     const { userId } = req.params;
-    const { paymentAmount, validUpto } = req.body;
-
+    const { membershipFee, validUpto } = req.body;
+    
     const userMembership = await Membership.findOne({
         userId: userId,
     })
@@ -24,21 +25,26 @@ export const updateMembership = async (req: Request, res: Response) => {
     const previousValidUpto = new Date(userMembership.validUpto);
     const validUptoDate = new Date(validUpto);
     const diffMonths = (validUptoDate.getFullYear() - previousValidUpto.getFullYear()) * 12 + (validUptoDate.getMonth() - previousValidUpto.getMonth());
-
+    
     userMembership.validUpto  = validUptoDate;
-    userMembership.membershipFee = paymentAmount;
+    userMembership.membershipFee = membershipFee;
     userMembership.membershipDuriation = diffMonths;
-
+    
     const transaction = await Transaction.create({
         userId,
         membershipId: userMembership?._id,
-        paymentAmount,
+        paymentAmount: membershipFee,
         paymentType: 'CASH',
     });
-
+    
     userMembership.lastPaymentDate = transaction.createdAt;
     userMembership.lastPaymentTransactionId = transaction._id;
-
-
-    return res.status(200)
+    userMembership.paymentStatus = 'PAID'
+    userMembership.save();
+    
+    
+    return res.status(200).json({error: false, message: 'Payment updated successfully'})
+} catch (error) {
+    return res.status(500).json({ error: true, message: "Couldn't update the payment" })
+}
 }
