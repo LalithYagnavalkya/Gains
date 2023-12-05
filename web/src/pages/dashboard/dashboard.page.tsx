9abf0fc73e5c3d948648b3e46308c39be9ee8af8
LalithYagnavalkya <lalluyagnavalkya.t@gmail.com>
2023-12-05 21:58:07 +0530

    🚀 Finialize dashboard layout

--- web/src/pages/dashboard/dashboard.page.tsx
+++ web/src/pages/dashboard/dashboard.page.tsx
@@ -98,10 +98,10 @@ export const Home = ({ className, ...props }: CardProps) => {
                                 </p>
                             </CardContent>
                         </Card>
-                        <Card className={cn("col-span-2", className)}>
+                        <Card>
                             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                                 <CardTitle className="text-sm font-medium">
-                                    Today
+                                    Upcoming due customers
                                 </CardTitle>
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -119,9 +119,36 @@ export const Home = ({ className, ...props }: CardProps) => {
                                 </svg>
                             </CardHeader>
                             <CardContent>
-                                <div className="text-2xl font-bold">{format(new Date(), 'do MMMM yyy')}</div>
+                                <div className="text-2xl font-bold">+2350</div>
                                 <p className="text-xs text-muted-foreground">
-                                    +180.1% from last month
+                                   Due date in next week
+                                </p>
+                            </CardContent>
+                        </Card>
+                        <Card className={cn("col-span-1", className)}>
+                            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                <CardTitle className="text-sm font-medium">
+                                    Today's Revenue
+                                </CardTitle>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    className="h-4 w-4 text-muted-foreground"
+                                >
+                                    <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                                    <circle cx="9" cy="7" r="4" />
+                                    <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+                                </svg>
+                            </CardHeader>
+                            <CardContent>
+                                <div className="text-2xl font-bold">₹4,200</div>
+                                <p className="text-xs text-muted-foreground">
+                                total transactions today
                                 </p>
                             </CardContent>
                         </Card>
