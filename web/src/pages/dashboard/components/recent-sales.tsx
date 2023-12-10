de738d2944e6805e2389fbf3c335e8ddcf101c9f
LalithYagnavalkya <lalluyagnavalkya.t@gmail.com>
2023-12-10 20:08:39 +0530

    🔨 Add null checks

--- web/src/pages/dashboard/components/recent-sales.tsx
+++ web/src/pages/dashboard/components/recent-sales.tsx
@@ -10,9 +10,9 @@ export function RecentSales(props: any) {
     const formatNumber = (num : number) => {
         let temp = String(num)
         // Remove any non-digit characters from the input (e.g., commas)
-        const sanitizedValue = temp.replace(/[^0-9]/g, '');
+        const sanitizedValue = temp?.replace(/[^0-9]/g, '');
         // Format the number with commas
-        return  sanitizedValue.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        return  sanitizedValue?.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     }
 
     return (
