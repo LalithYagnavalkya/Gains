f3ca39fae56d4425d5b358d5c793a90659c29370
Lalith Yagnavalkya Tirunagari <y.lalith01@gmail.com>
2023-12-10 13:50:36 +0530

    🔧 API integration

--- web/src/pages/dashboard/components/overview.tsx
+++ web/src/pages/dashboard/components/overview.tsx
@@ -53,26 +53,34 @@ const data = [
     },
 ]
 
-export function Overview() {
-    return (
-        <ResponsiveContainer width="100%" height={350}>
-            <BarChart data={data}>
-                <XAxis
-                    dataKey="name"
-                    stroke="#888888"
-                    fontSize={12}
-                    tickLine={false}
-                    axisLine={false}
-                />
-                <YAxis
-                    stroke="#888888"
-                    fontSize={12}
-                    tickLine={false}
-                    axisLine={false}
-                    tickFormatter={(value: any) => `$${value}`}
-                />
-                <Bar dataKey="total" fill="#adfa1d" radius={[4, 4, 0, 0]} />
-            </BarChart>
-        </ResponsiveContainer>
-    )
+export function Overview(props: any) {
+    if (props?.data) {
+
+        return (
+            <ResponsiveContainer width="100%" height={350}>
+                <BarChart data={props?.data}>
+                    <XAxis
+                        dataKey="name"
+                        stroke="#888888"
+                        fontSize={12}
+                        tickLine={false}
+                        axisLine={false}
+                    />
+                    <YAxis
+                        stroke="#888888"
+                        fontSize={12}
+                        tickLine={false}
+                        axisLine={false}
+                        tickFormatter={(value: any) => `$${value}`}
+                    />
+                    <Bar dataKey="total" fill="#adfa1d" radius={[4, 4, 0, 0]} />
+                </BarChart>
+            </ResponsiveContainer>
+        )
+
+    } else {
+
+        return <></>
+    }
+
 }
