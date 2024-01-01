7408993dddde8a7fe49bdf9bf6da3c837be483bd
Lalith <y.lalith01@gmail.com>
2024-01-01 13:03:13 +0530

    ⚡ Update membership scheduler

--- server/admin-service/src/controllers/scheduler.controller.ts
+++ server/admin-service/src/controllers/scheduler.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 const schedule = require('node-schedule');
 import schedulerLog from "../models/scheduler.log.model";
-import Membership from "../models/membership.model";
+import Membership, { IMembership } from "../models/membership.model";
 
 export const runMembershipScheduler = async (req: Request, res: Response) => {
 
@@ -13,63 +13,48 @@ export const runMembershipScheduler = async (req: Request, res: Response) => {
 // }
 
 
-// const memberShipJob = schedule.scheduleJob('0 0 * * *', async function () {
-//     let scheduler_id;
-//     try {
-//         let today = new Date();
-//         let todayStart = today.setHours(0, 0, 0, 0);
-//         let todayEnd = today.setHours(23, 59, 0, 0);
+const memberShipJob = schedule.scheduleJob('0 0 * * *', async function () {
+    let scheduler_id :string;
+    try {
+        let today = new Date();
+        let todayStart = today.setHours(0, 0, 0, 0);
+        let todayEnd = today.setHours(23, 59, 0, 0);
 
-//         const checkIfAnyPreviousProcess = await schedulerLog.findOne({
-//             type: 'MEMBERSHIP', status: 'In Progress',
-//             $and: [
-//                 {
-//                     startDate: { $lte: todayEnd }
-//                 },
-//                 {
-//                     startDate: { $gte: todayStart }
-//                 }
-//             ]
-//         });
+        let _scheduler = await schedulerLog.create({ type: 'MEMBERSHIP', status: 'In Progress', startDate: new Date() });
+        scheduler_id = String(_scheduler._id)
+        setTimeout(async () => {
+            await membershipJobLogic(scheduler_id);
+        }, 10000)
 
-//         if (checkIfAnyPreviousProcess) {
-//             await schedulerLog.create({
-//                 type: 'MEMBERSHIP',
-//                 status: 'already runing',
-//                 startTime: new Date(),
-//             })
-//             return 
-//         }
+    } catch (error: any) {
 
-//         scheduler_id = await schedulerLog.create({ type: 'MEMBERSHIP', status: 'In Progress', startDate: new Date() });
+    }
 
-//         // await membershipJobLogic();
+});
 
-//     } catch (error: any) {
-
-//     }
-
-// });
-
-const membershipJobLogic = async () => {
+const membershipJobLogic = async (scheduler_id: string) => {
     try {
         const currentDate = new Date();
-        Membership.updateMany({}, {validUpto: new Date})
-        const memberShips = await Membership.find( {
+        const threeDaysInFuture = new Date(new Date().setDate(new Date().getDate() + 3));
+        const memberShips = await Membership.find({
             validUpto: {
-                $lte: currentDate,
+                $lte: threeDaysInFuture.setHours(23, 59, 0, 0),
             },
-            paymentStatus: { $ne: 'PAID' },
+            paymentStatus: { $ne: 'PAST_DUE' },
         })
 
-        const x = memberShips.map(x => {
-            x.paymentStatus = 'PENDING'
-            return x.save();
+        memberShips.forEach(async (m: IMembership) => {
+            if (m.validUpto < currentDate) {
+                // validUpto is expired
+                m.paymentStatus = 'PENDING';
+            } else if (new Date(m.validUpto).setHours(23, 59, 0, 0) === threeDaysInFuture.getTime()) {
+                m.paymentStatus = 'UPCOMMING';
+            }
         })
 
     } catch (error) {
-        
+        await schedulerLog.findByIdAndUpdate({ _id: scheduler_id }, { status: 'Failed', error });
     }
 }
 
-membershipJobLogic();
+// membershipJobLogic();
